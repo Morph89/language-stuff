900df1baf279aa6ec35d75afe8c5148092856bd2
Morph89 <mate.kertesz@icloud.com>
2019-05-30 10:01:05 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
 
-  cards:any[] = [
-    {},{},{},{},{},{},{},{},{},{},{},{}
-  ]
+  cards: any[] = [
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false },
+    { active: false, turned: false }
+  ];
+
+  getCardClass(card: any) {
+    if(card.turned === true) {
+      let val = 'turned';
+      return card.active ? val + ' active' : val;
+    }
+  }
+
+  onCardClick(card: any) {
+    console.log('Click on card');
+    card.turned = true;
+    
+    setTimeout(() => {
+      card.active = true;
+    }, 500);
+  }
 }
